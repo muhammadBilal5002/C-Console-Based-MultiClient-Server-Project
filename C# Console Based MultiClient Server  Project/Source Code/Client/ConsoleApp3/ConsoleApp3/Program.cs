f16e4cc789ce6eacfe2f00f05cf4c0fbac361c2a
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-06-23 12:58:57 +0500

    First Version

---
+++ C# Console Based MultiClient Server  Project/Source Code/Client/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+
+	class Program
+	{
+		static string myname = "";
+		static Boolean flag = false;
+		static void Main(string[] args)
+		{
+			IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+			Socket CilentSocket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			CilentSocket.Connect(localEndPoint);
+			// Registration
+			Console.WriteLine("Enter Your Name");
+			while (true)
+			{
+				string nameofclient = null;
+				nameofclient = Console.ReadLine();
+				CilentSocket.Send(Encoding.ASCII.GetBytes(nameofclient), 0, nameofclient.Length, SocketFlags.None);
+				byte[] msg = new Byte[1024];
+				int size = CilentSocket.Receive(msg);
+				string value = Encoding.ASCII.GetString(msg, 0, size);
+				if (value == "Ok")
+				{
+					myname = nameofclient.ToLower();
+					break;
+				}
+				Console.WriteLine("Message from Server -> {0}", value);
+				Console.WriteLine("Enter Your Name Again");
+			}
+			Console.WriteLine("You Are Registered");
+			Console.WriteLine("\nEnter Your Friend Name");
+			//Connection With Friend
+			CON_FRIEND(CilentSocket);
+		}
+		public static void CON_FRIEND(Socket CilentSocket)
+		{
+			while (true)
+			{
+				string nameoffriend = null;
+				nameoffriend = Console.ReadLine();
+				if (nameoffriend.ToLower() == myname)
+				{
+					Console.WriteLine("Please Dont Write Your Name");
+					Console.WriteLine("Enter Your Friend Name Again");
+				}
+				else
+				{
+					CilentSocket.Send(Encoding.ASCII.GetBytes(nameoffriend), 0, nameoffriend.Length, SocketFlags.None);
+					byte[] msg = new Byte[1024];
+					int size = CilentSocket.Receive(msg);
+					string value = Encoding.ASCII.GetString(msg, 0, size);
+					if (value == "Friend Not Found")
+					{
+						Console.WriteLine("\nMessage from Server -> {0}", value);
+						Console.WriteLine("\nEnter Your Friend Name Again");
+					}
+					else if (value == "Your Friend Is Busy With Other")
+					{
+						Console.WriteLine("\nMessage from Server -> {0}", value);
+						Console.WriteLine("\nEnter Another Friend Name or Try Later");
+					}
+					else if (value == "Your Friend Is Waiting For Other") {
+						Console.WriteLine("\nMessage from Server -> {0}" ,value);
+						Console.WriteLine("\nEnter Another Friend Name or Try Later");
+					}
+					else if (value == "Waiting For Your Friend Connection")
+					{
+						Console.WriteLine("\nMessage from Server -> {0}", value);
+						byte[] wait = new Byte[1024];
+						int wait_size = CilentSocket.Receive(wait);
+						string wait_value = Encoding.ASCII.GetString(wait, 0, wait_size);
+						if (wait_value == "GOOD")
+						{
+							CilentSocket.Send(Encoding.ASCII.GetBytes("OK"));
+							flag = true;
+							break;
+						}
+						else if (wait_value == "Your Friend Want To Chat With Other")
+						{
+							CilentSocket.Send(Encoding.ASCII.GetBytes("NOT_OK"));
+							Console.WriteLine("\nMessage from Server -> {0}", wait_value);
+							Console.WriteLine("\nEnter Another Friend Name or Try Later");
+						}
+                        else {
+						CilentSocket.Send(Encoding.ASCII.GetBytes("NOT_OK"));
+						Console.WriteLine("\nMessage from Server -> Your Friend Is Connected With Other");
+						Console.WriteLine("\nEnter Another Friend Name or Try Later");
+					}
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			Console.WriteLine("\n********************You Are Connected With Your Friend*******************");
+			CHATING(CilentSocket);
+		}
+
+		public static void CHATING(Socket CilentSocket)
+		{
+			if (flag) {
+				byte[] msg = new Byte[1024];
+				int size = CilentSocket.Receive(msg);
+				Console.WriteLine(Encoding.ASCII.GetString(msg, 0, size));
+			}
+			while (true)
+			{
+				string messageFromClient = null;
+				Console.WriteLine("Enter The Message");
+				messageFromClient = Console.ReadLine();
+				CilentSocket.Send(Encoding.ASCII.GetBytes(messageFromClient), 0, messageFromClient.Length, SocketFlags.None);
+				if (messageFromClient == "END") {
+					flag = false;
+					Console.WriteLine("\n*************** CHAT ENDED **************");
+					Console.WriteLine("\nEnter Your Friend Name");
+					break;
+
+				}
+				byte[] msg = new Byte[1024];
+				int size = CilentSocket.Receive(msg);
+				Console.WriteLine(Encoding.ASCII.GetString(msg, 0, size));
+				String messageToClient = Encoding.ASCII.GetString(msg, 0, size);
+				if (messageToClient == "END")
+				{
+					CilentSocket.Send(Encoding.ASCII.GetBytes("END1"));
+					flag = false;
+					Console.WriteLine("\n*************** CHAT ENDED **************");
+					Console.WriteLine("\nEnter Your Friend Name");
+					break;
+
+				}
+			}
+			//Connection With Friend
+			CON_FRIEND(CilentSocket);
+		}
+	}
+}
---
+++ C# Console Based MultiClient Server  Project/Source Code/Server/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Server
+{
+	class Program
+	{
+		static ArrayList name_arr = new ArrayList();
+		static List<Socket> socket_arr = new List<Socket>();
+		static ArrayList Request_From = new ArrayList();
+		static ArrayList Request_To = new ArrayList();
+		static ArrayList Confirm_From = new ArrayList();
+		static ArrayList Confirm_To = new ArrayList();
+		static void Main(string[] args)
+		{
+			IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+			Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			listener.Bind(localEndPoint);
+			listener.Listen(10);
+			Console.WriteLine("Server Is Running ... ");
+			Socket clientSocket = default(Socket);
+			Program p = new Program();
+			while (true)
+			{
+				clientSocket = listener.Accept();
+				Thread newthread = new Thread(new ThreadStart(() => p.User(clientSocket)));
+				newthread.Start();
+
+			}
+		}
+		public void User(Socket Client)
+		{   //user name added into name array
+			string myname;
+			string friendname;
+				while (true)
+				{
+					byte[] name = new Byte[1024];
+					int name_len = Client.Receive(name);
+					string strname = Encoding.ASCII.GetString(name, 0, name_len);
+					strname = strname.Trim();
+					strname = strname.ToLower();
+					if (!name_arr.Contains(strname))
+					{
+						name_arr.Add(strname);
+						socket_arr.Add(Client);
+						myname = strname;
+						Client.Send(Encoding.ASCII.GetBytes("Ok"));
+						break;
+					}
+					else
+					{
+						Client.Send(Encoding.ASCII.GetBytes("User Already Exist"));
+					}
+
+				}
+
+			while (true)
+			{
+				Boolean stopuser = false;
+				Socket FriendSocket = default(Socket);
+				Socket MySocket = default(Socket);
+				// checking friend validatio
+				while (true)
+				{
+					byte[] friend = new Byte[1024];
+					int friend_len = Client.Receive(friend);
+					friendname = Encoding.ASCII.GetString(friend, 0, friend_len);
+					friendname = friendname.Trim();
+					friendname = friendname.ToLower();
+					if (!name_arr.Contains(friendname))
+					{
+						Client.Send(Encoding.ASCII.GetBytes("Friend Not Found"));
+					}
+					else if (Confirm_From.Contains(friendname) || Confirm_To.Contains(friendname))
+					{
+						Client.Send(Encoding.ASCII.GetBytes("Your Friend Is Busy With Other"));
+					}
+					else
+					{
+						Boolean flag = false;
+						Boolean flag2 = false;
+						string[] indexer = new string[10];
+						Console.WriteLine(Request_To.Count);
+						for (int i = 0; i < Request_To.Count; i++)
+						{
+							if (Request_From[i].Equals(friendname))
+							{
+								flag2 = true;
+								if (Request_To[i].Equals(myname))
+								{
+									Request_From.RemoveAt(i);
+									Request_To.RemoveAt(i);
+									while (true)
+									{
+										int index = -1;
+										index = Request_To.IndexOf(myname);
+										if (index == -1)
+										{
+											break;
+										}
+										socket_arr[name_arr.IndexOf(Request_From[index])].Send(Encoding.ASCII.GetBytes("Not Ok"));
+										Request_From.RemoveAt(index);
+										Request_To.RemoveAt(index);
+									}
+									
+									Confirm_From.Add(friendname);
+									Confirm_To.Add(myname);
+									FriendSocket = socket_arr[name_arr.IndexOf(friendname)];
+									MySocket = socket_arr[name_arr.IndexOf(myname)];
+									socket_arr[name_arr.IndexOf(friendname)].Send(Encoding.ASCII.GetBytes("GOOD"));
+									Client.Send(Encoding.ASCII.GetBytes("OK"));
+									flag = true;
+									flag2 = false;
+								}
+								else
+								{
+									Client.Send(Encoding.ASCII.GetBytes("Your Friend Is Waiting For Other"));
+								}
+								break;
+							}
+						}
+						if (flag)
+						{
+							break;
+						}
+						else if (!flag2)
+						{
+							Request_From.Add(myname);
+							Request_To.Add(friendname);
+							while (true)
+							{
+								int index = -1;
+								index = Request_To.IndexOf(myname);
+								if (index == -1)
+								{
+									break;
+								}
+								socket_arr[name_arr.IndexOf(Request_From[index])].Send(Encoding.ASCII.GetBytes("Your Friend Want To Chat With Other"));
+								Request_From.RemoveAt(index);
+								Request_To.RemoveAt(index);
+							}
+							Client.Send(Encoding.ASCII.GetBytes("Waiting For Your Friend Connection"));
+							byte[] okornot = new Byte[1024];
+							int okornot_len = Client.Receive(okornot);
+							string strokornot = Encoding.ASCII.GetString(okornot, 0, okornot_len);
+							Console.WriteLine(strokornot == "OK");
+							if (strokornot == "OK")
+							{
+								FriendSocket = socket_arr[name_arr.IndexOf(friendname)];
+								MySocket = socket_arr[name_arr.IndexOf(myname)];
+								break;
+							}
+						}
+					}
+				}
+
+				while (true)
+				{
+					byte[] msg = new Byte[1024];
+					int size = MySocket.Receive(msg);
+					string value = Encoding.ASCII.GetString(msg, 0, size);
+					if (value == "END")
+					{
+						FriendSocket.Send(Encoding.ASCII.GetBytes("END"));
+						Confirm_From.Remove(friendname);
+						Confirm_To.Remove(myname);
+						break;
+					}
+					if (value == "END1")
+					{
+						break;
+					}
+					Console.WriteLine("From: " + myname + "  To: " + friendname + "==>" + value);
+					value = "From " + myname + "-->  " + value;
+					FriendSocket.Send(Encoding.ASCII.GetBytes(value));
+				}
+			}
+		}
+		}
+	}
